51e85a4ebfaae84afe826a3d664db8abd1cbab43
Sujan Shrestha <sujanshrestha.net@gmail.com>
2023-06-02 09:41:11 +0545

    route changes

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// Route::get('paywithpaypal', array('as' => 'paywithpaypal','uses' => 'PaypalController@payWithPaypal',));
-// Route::post('paypal', array('as' => 'paypal','uses' => 'PaypalController@postPaymentWithpaypal',));
-// Route::get('paypal', array('as' => 'status','uses' => 'PaypalController@getPaymentStatus',));
-
-
-// Route::get('/paywithpaypal', [PaypalController::class, 'payWithPaypal']);
-// Route::post('paypal', [PaypalController::class, 'postPaymentWithpaypal']);
-// Route::get('paypal', array('as' => 'status'), [PaypalController::class, 'getPaymentStatus']);
-
-
-// Route::get('paywithpaypal', 'PaypalController@payWithPaypal')->name('paywithpaypal');
-// Route::post('paypal', 'PaypalController@postPaymentWithpaypal')->name('paypal');
-// Route::get('paypal', 'PaypalController@getPaymentStatus')->name('status');
-
-
-
 Route::get('paywithpaypal', array('as' => 'paywithpaypal','uses' => 'App\Http\Controllers\PaypalController@payWithPaypal',));
 Route::post('paypal', array('as' => 'paypal','uses' => 'App\Http\Controllers\PaypalController@postPaymentWithpaypal',));
 Route::get('paypal', array('as' => 'status','uses' => 'App\Http\Controllers\PaypalController@getPaymentStatus',));
